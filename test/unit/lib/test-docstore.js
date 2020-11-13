6fdaa3be9ac68dcf13798e3a0b5fdabfb29507ac
Kent Lee <kent@gallerymediagroup.com>
2020-11-13 16:38:11 -0500

    adding mocha unit tests to eslint config

--- test/unit/lib/test-docstore.js
+++ test/unit/lib/test-docstore.js
@@ -1,29 +1,27 @@
-'use strict';
+'use strict'
 
 const moment = require('moment-timezone')
 
 describe('Test docstore:', function () {
+  const expect = require('chai').expect
+  const docstore = require('../../../lib/docstore.js')
 
-    const expect = require('chai').expect;
-    const docstore = require('../../../lib/docstore.js');
+  const stamp = moment().tz('UTC').toString()
 
-    const stamp = moment().tz('UTC').toString();
+  it('should get 0', function () {
+    expect(docstore.getKey(stamp)).to.equal(0)
+  })
 
-    it('should get 0', function () {
-        expect(docstore.getKey(stamp)).to.equal(0);
-    });
+  it('should save stamp', function () {
+    expect(docstore.setKey(stamp)).to.equal('Saved ' + stamp)
+  })
 
-    it('should save stamp', function () {
-        expect(docstore.setKey(stamp)).to.equal('Saved ' + stamp);
-    });
+  it('should get 1', function () {
+    expect(docstore.getKey(stamp)).to.equal(1)
+  })
 
-    it('should get 1', function () {
-        expect(docstore.getKey(stamp)).to.equal(1);
-    });
-
-    it('should get 5', function () {
-        for (let i = 0;  i < 4; i++) docstore.setKey(stamp);
-        expect(docstore.getKey(stamp)).to.equal(5);
-    });
-
-});
+  it('should get 5', function () {
+    for (let i = 0; i < 4; i++) docstore.setKey(stamp)
+    expect(docstore.getKey(stamp)).to.equal(5)
+  })
+})
