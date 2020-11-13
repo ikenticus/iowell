48650bb0d5f54432271682d6371cfe6777c2c667
Kent Lee <kent@gallerymediagroup.com>
2020-11-13 16:25:26 -0500

    adding some basic unit-test for docstore as well as code coverage

---
+++ test/unit/lib/test-docstore.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const moment = require('moment-timezone')
+
+describe('Test docstore:', function () {
+
+    const expect = require('chai').expect;
+    const docstore = require('../../../lib/docstore.js');
+
+    const stamp = moment().tz('UTC').toString();
+
+    it('should get 0', function () {
+        expect(docstore.getKey(stamp)).to.equal(0);
+    });
+
+    it('should save stamp', function () {
+        expect(docstore.setKey(stamp)).to.equal('Saved ' + stamp);
+    });
+
+    it('should get 1', function () {
+        expect(docstore.getKey(stamp)).to.equal(1);
+    });
+
+    it('should get 5', function () {
+        for (let i = 0;  i < 4; i++) docstore.setKey(stamp);
+        expect(docstore.getKey(stamp)).to.equal(5);
+    });
+
+});
